f5f909ccc6e5dd107e4e83463cb826ed2ae0077d
mucephei <pparodiramirez@gmail.com>
2022-06-29 19:22:59 -0300

    Desafio - Importacion de datos

--- create.sql
+++ create.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA IF NOT EXISTS ipc_argentina;
 USE ipc_argentina;
 
 CREATE TABLE  IF NOT EXISTS periodo(
-id_periodo INT NOT NULL,
+id_periodo INT NOT NULL AUTO_INCREMENT,
 mes_nombre TEXT(15),
 mes INT NOT NULL,
 año INT NOT NULL,
@@ -15,45 +15,70 @@ region TEXT(10),
 PRIMARY KEY (id_region)
 );
 
-CREATE TABLE IF NOT EXISTS ipc(
-id_ipc INT NOT NULL,
-valor_ipc_intermensual DECIMAL(4,2),
-valor_ipc_interanual DECIMAL(4,2),
-valor_ipc_acumulado DECIMAL(4,2),
-id_periodo INT NOT NULL,
-PRIMARY KEY (id_ipc),
-FOREIGN KEY (id_periodo) REFERENCES periodo(id_periodo)
-);
-
 CREATE TABLE IF NOT EXISTS divisiones(
 id_division INT NOT NULL AUTO_INCREMENT,
 division TEXT(50),
-valor_ipc_division DECIMAL(4,2),
-id_periodo INT NOT NULL,
-id_region INT NOT NULL,
-PRIMARY KEY (id_division),
-FOREIGN KEY (id_periodo) REFERENCES periodo(id_periodo),
-FOREIGN KEY (id_region) REFERENCES region(id_region)
+PRIMARY KEY (id_division)
 );
 
 CREATE TABLE IF NOT EXISTS aperturas(
 id_apertura INT NOT NULL AUTO_INCREMENT,
 apertura TEXT(50),
-valor_ipc_apertura DECIMAL(4,2),
+id_division INT NOT NULL,
+PRIMARY KEY (id_apertura),
+FOREIGN KEY (id_division) REFERENCES divisiones(id_division)
+);
+
+/*CREATE TABLE IF NOT EXISTS productos(
+id_producto INT NOT NULL AUTO_INCREMENT,
+producto TEXT(50),
+id_apertura INT NOT NULL,
+PRIMARY KEY (id_producto)
+);*/
+
+CREATE TABLE IF NOT EXISTS ipc(
+id_ipc INT NOT NULL AUTO_INCREMENT,
+valor_ipc_intermensual DECIMAL(8,2),
+valor_ipc_interanual DECIMAL(8,2),
 id_periodo INT NOT NULL,
 id_region INT NOT NULL,
-PRIMARY KEY (id_apertura),
+PRIMARY KEY (id_ipc),
 FOREIGN KEY (id_periodo) REFERENCES periodo(id_periodo),
 FOREIGN KEY (id_region) REFERENCES region(id_region)
 );
 
-CREATE TABLE IF NOT EXISTS productos(
-id_productos INT NOT NULL AUTO_INCREMENT,
-productos TEXT(50),
-valor_ipc_productos DECIMAL(4,2),
+CREATE TABLE IF NOT EXISTS ipc_divisiones(
+id_ipc_division INT NOT NULL AUTO_INCREMENT,
+valor_ipc_division DECIMAL(8,2),
+id_division INT NOT NULL,
 id_periodo INT NOT NULL,
 id_region INT NOT NULL,
-PRIMARY KEY (id_productos),
+PRIMARY KEY (id_ipc_division),
+FOREIGN KEY (id_division) REFERENCES divisiones(id_division),
 FOREIGN KEY (id_periodo) REFERENCES periodo(id_periodo),
 FOREIGN KEY (id_region) REFERENCES region(id_region)
 );
+
+CREATE TABLE IF NOT EXISTS ipc_aperturas(
+id_ipc_apertura INT NOT NULL AUTO_INCREMENT,
+valor_ipc_apertura DECIMAL(8,2),
+id_apertura INT NOT NULL,
+id_periodo INT NOT NULL,
+id_region INT NOT NULL,
+PRIMARY KEY (id_ipc_apertura),
+FOREIGN KEY (id_apertura) REFERENCES aperturas(id_apertura),
+FOREIGN KEY (id_periodo) REFERENCES periodo(id_periodo),
+FOREIGN KEY (id_region) REFERENCES region(id_region)
+);
+
+/*CREATE TABLE IF NOT EXISTS ipc_productos(
+id_ipc_producto INT NOT NULL AUTO_INCREMENT,
+valor_ipc_producto DECIMAL(8,2),
+id_producto INT NOT NULL,
+id_periodo INT NOT NULL,
+id_region INT NOT NULL,
+PRIMARY KEY (id_ipc_producto),
+FOREIGN KEY (id_producto) REFERENCES productos(id_producto),
+FOREIGN KEY (id_periodo) REFERENCES periodo(id_periodo),
+FOREIGN KEY (id_region) REFERENCES region(id_region)
+);*/
--- query.sql
+++ query.sql
@@ -1,6 +1,27 @@
 /*DROP SCHEMA ipc_argentina;*/
 
-SELECT p.mes_nombre as mes, p.año, i.valor_ipc_intermensual, i.valor_ipc_interanual, i.valor_ipc_acumulado
-FROM ipc as i
+/*SELECT * FROM periodo;
+SELECT * FROM region;
+SELECT * FROM divisiones;
+SELECT * FROM aperturas;
+SELECT * FROM ipc_divisiones;
+SELECT * FROM ipc_aperturas;*/
+
+SELECT * FROM ipc;
+SELECT d.id_ipc_division,
+(SELECT division FROM divisiones WHERE d.id_division = id_division) as division,
+d.valor_ipc_division, p.mes_nombre as mes, p.año,
+(SELECT region FROM region WHERE d.id_region = id_region) as region
+FROM ipc_divisiones as d
 JOIN periodo as p
-ON (i.id_ipc = p.id_periodo);
+ON p.id_periodo = d.id_periodo
+ORDER BY valor_ipc_division DESC;
+
+SELECT a.id_ipc_apertura,
+(SELECT apertura FROM aperturas WHERE a.id_apertura = id_apertura) as apertura,
+a.valor_ipc_apertura, p.mes_nombre as mes, p.año,
+(SELECT region FROM region WHERE a.id_region = id_region) as region
+FROM ipc_aperturas as a
+JOIN periodo as p
+ON (p.id_periodo = a.id_periodo && p.año >= 2021)
+ORDER BY valor_ipc_apertura DESC;
